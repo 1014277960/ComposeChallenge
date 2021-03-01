bce555b0b717a3151726f08d96c6dfac887c6ee2
wulinpeng <wulinpeng@bytedance.com>
2021-03-01 14:46:32 +0800

    fix problem with lint

--- app/src/main/java/com/example/androiddevchallenge/ui/MyApp.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/MyApp.kt
@@ -1,11 +1,16 @@
 package com.example.androiddevchallenge.ui
 
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
